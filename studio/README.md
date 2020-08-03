09e7fd6fca275bfd3d4f38750b350a77eb879ad2
Sanity.io <hello@sanity.io>
2020-08-03 17:24:12 +0200

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+sanity-kitchen-sink-256-studio
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "a8fcv3iz",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production"
+  }
+};
